4eaf634a48f4e273057d4a45b63a9940f06765d4
windstruck <nay_issada@hotmail.com>
2013-10-02 21:53:26 +0700

    ทำให้ใช้งานได้

--- check_prime.php
+++ check_prime.php
@@ -4,11 +4,37 @@
  * 
  */
 
-// definitely not a prime
-echo gmp_prob_prime("6") . "\n";
+function isPrime($n)
+{
+	$i = 2;
+ 
+	if ($n == 2) {
+		return true;	
+	}
+ 
+	while ($i <= $n/2) {
+		if ($n % $i == 0) {
+			return false;
+		}
+		$i++;
+	}
+ 
+	return true;
+}
 
-// probably a prime
-echo gmp_prob_prime("1111111111111111111") . "\n";
+/* if(isPrime(3)){
+	echo 'is prime';
+}else {
+	echo 'not prime';
+} */
+if(isPrime($_POST['send_num'])){
+	echo 'is prime';
+}else {
+	echo 'not prmie';
+}
+?>
 
-// definitely a prime
-echo gmp_prob_prime("11") . "\n";
+<form method="post">
+	<input type="text" name="send_num">
+	<input type="submit" valut="submit">
+</form>
